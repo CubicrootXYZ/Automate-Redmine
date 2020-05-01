984065db9f905b44432ea1b3621792d1aecb41db
Cubicroot <24812442+CubicrootXYZ@users.noreply.github.com>
2020-05-01 20:53:16 +0200

    .

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
 from redminelib import Redmine
 import re
+import configparser,os
 
+os.chdir("D:/github_repos/Automate-Redmine")
 
+config = configparser.ConfigParser()
+config.readfp(open('settings.ini'))
+url = config.get('redmine', 'url')
+username = config.get('redmine', 'user')
+password = config.get('redmine', 'password')
 
 close_tags = ["Hallo .* Alexander"]
 
-redmine = Redmine(url, username=admin, password=password)
+redmine = Redmine(url, username=username, password=password)
 project = redmine.project.get('test-ticket')
 
 for issue in project.issues:
